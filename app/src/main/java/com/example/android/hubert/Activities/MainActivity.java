e037fa233a0692520a947b56a73e2b48f60b52c6
Njuacha <betomis2006@yahoo.com>
2018-08-16 05:12:24 +0100

    Add on click reactions to Contributions tab.
    To enable view of details, editing of name, view of summary and delete.

--- app/src/main/java/com/example/android/hubert/Activities/MainActivity.java
+++ app/src/main/java/com/example/android/hubert/Activities/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.android.hubert.Activities;
 
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
+import android.content.Intent;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
 import android.support.design.widget.FloatingActionButton;
@@ -9,6 +10,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.PopupMenu;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 
@@ -27,9 +29,11 @@ import android.widget.TextView;
 
 import com.example.android.hubert.Adapters.ContributionsAdapter;
 import com.example.android.hubert.Adapters.MembersAdapter;
+import com.example.android.hubert.AppExecutors;
 import com.example.android.hubert.DatabaseClasses.A_list;
 import com.example.android.hubert.DatabaseClasses.AppDatabase;
 import com.example.android.hubert.DatabaseClasses.Member;
+import com.example.android.hubert.DialogFragments.Listname_dialog;
 import com.example.android.hubert.R;
 import com.example.android.hubert.View_model_classes.ContributionsViewModel;
 import com.example.android.hubert.View_model_classes.MembersViewModel;
@@ -54,7 +58,12 @@ public class MainActivity extends AppCompatActivity {
      * The {@link ViewPager} that will host the section contents.
      */
     private ViewPager mViewPager;
-    private AppDatabase mDb;
+    private static AppDatabase mDb;
+
+    public static final String LIST_ID_EXTRA = "list Id";
+    public static final String LIST_NAME_EXTRA = "list name";
+    private static final String DEFAULT_LIST_NAME = "No Name";
+    public final static int DEFAULT_LIST_ID = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -123,6 +132,7 @@ public class MainActivity extends AppCompatActivity {
         private static final String ARG_SECTION_NUMBER = "section_number";
         private static final int MEMBERS_SECTION = 1 ;
         private static final int CONTRIBUTIONS_SECTION = 2;
+
 
         public PlaceholderFragment() {
         }
@@ -207,17 +217,57 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void onItemCLicked(int itemId, String name) {
-
+            Intent intent = new Intent(getActivity(), Display_a_list.class);
+            intent.putExtra(LIST_NAME_EXTRA, name);
+            intent.putExtra(LIST_ID_EXTRA, itemId);
+            startActivity(intent);
         }
 
         @Override
         public void onItemLongClicked(int itemId, String name) {
-
+            open_edit_list_name_dialog(itemId, name);
         }
 
         @Override
-        public void onOptionTextViewClicked(int itemId, String name, View view) {
+        public void onOptionTextViewClicked(final int itemId, String name, View view) {
+            final int listId = itemId;
+            final String listName = name;
+            PopupMenu popupMenu = new PopupMenu(getContext(),view);
+            popupMenu.inflate(R.menu.list);
+            popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                @Override
+                public boolean onMenuItemClick(MenuItem item) {
+                    switch (item.getItemId()){
+                        case R.id.action_details:
+                            Intent showSummaryIntent = new Intent(getActivity(),SummaryActivity.class);
+                            showSummaryIntent.putExtra(LIST_ID_EXTRA,listId);
+                            showSummaryIntent.putExtra(LIST_NAME_EXTRA,listName);
+                            startActivity(showSummaryIntent);
+                            break;
+                        case R.id.action_delete:
+                            AppExecutors.getsInstance().diskIO().execute(new Runnable() {
+                                @Override
+                                public void run() {
+                                    mDb.a_list_dao().delete_a_list(itemId);
+                                }
+                            });
+                            break;
+                    }
+                    return false;
+                }
+            });
 
+            popupMenu.show();
+        }
+
+        private void open_edit_list_name_dialog(int listId, String name){
+
+            Listname_dialog dialog = new Listname_dialog();
+            Bundle bundle = new Bundle();
+            bundle.putInt(LIST_ID_EXTRA,listId);
+            bundle.putString(LIST_NAME_EXTRA,name);
+            dialog.setArguments(bundle);
+            dialog.show(getFragmentManager(), "edit_list_name_dialog");
         }
     }
 
